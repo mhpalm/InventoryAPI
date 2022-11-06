c21296780af873f3b1339c74e30b2b29f30c115d
Micah Palmquist <micah.palmquist@gmail.com>
2022-11-05 21:32:09 -0400

    Add project files.

---
+++ InventoryAPI/Data/ItemDbContext.cs
@@ -0,0 +1,13 @@
+﻿using InventoryAPI.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace InventoryAPI.Data
+{
+    public class ItemDbContext : DbContext
+    {
+        public DbSet<Item> Items { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder options)
+            => options.UseSqlite(@"Data Source=C:\Temp\items.db");
+    }
+}
---
+++ InventoryAPI/Models/Item.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace InventoryAPI.Models
+{
+    public class Item
+    {
+        public int Id { get; set; }
+        [Required]
+        public string ItemName { get; set; }
+        public double ItemPrice { get; set; }
+        public string ItemLocation { get; set; }
+    }
+}
